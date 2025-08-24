859782a7c2e78f0edf4cf5539cab291b907d6d5b
deezinn <developer.andrelz@gmail.com>
2025-08-24 13:20:38 -0300

    feat(style): new compontent

--- easy-english/src/app/page.jsx
+++ easy-english/src/app/page.jsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
 import personIcon from "@/assets/images/person_icon/person.jpeg";
+import { Icon } from "@iconify/react";
+
+
 export default function Home() {
   return (
     <>
@@ -12,9 +15,9 @@ export default function Home() {
           {/* Grade da direita */}
           <div className="w-full h-auto p-5 ">
             <div className="w-full h-auto grid grid-rows-3 gap-5">
-              <div className="w-full h-auto flex flex-col text-white bg-black p-5 gap-12 rounded-2xl">
+              <div className="w-full h-auto flex flex-col text-white bg-gradient-to-r from-black/80 from-10% to-black to-90% p-5 gap-12 rounded-2xl">
                 {/* Parte de cima */}
-                <div className="w-full h-20 grid grid-cols-3 justify-center items-center">
+                <div className="w-full h-20 grid grid-cols-[1fr_2fr_1fr]">
                   <div className="w-full h-auto flex justify-center items-center ">
                     <div className="relative w-20 h-20">
                       <Image
@@ -24,27 +27,32 @@ export default function Home() {
                       />
                     </div>
                   </div>
-                  <div className="w-full h-auto flex flex-col justify-center">
+                  <div className="w-full h-auto flex flex-col">
                     <h1 className="text-4xl">André Luiz</h1>
-                    <h1 className="text-2xl">Developer</h1>
+                    <h1 className="text-md opacity-50">Developer</h1>
                   </div>
                   <div className="w-full h-auto flex justify-center mr-10">
-                    <h1 className="text-6xl">...</h1>
+                    <Icon
+                      icon="svg-spinners:3-dots-move"
+                      width="60"
+                      height="60"
+                      className="text-white"
+                    />
                   </div>
                 </div>
 
                 <div className="w-full h-auto flex flex-row justify-evenly items-center relative ">
                   <div className="flex flex-col">
-                    <h1>teste1</h1>
-                    <h1>teste1</h1>
+                    <h1 className="text-gray-400">Score</h1>
+                    <h1 className="text-5xl">9.7</h1>
                   </div>
                   <div className="flex flex-col">
-                    <h1>teste1</h1>
-                    <h1>teste1</h1>
+                    <h1 className="text-gray-400">Investimento</h1>
+                    <h1 className="text-5xl">10.5K</h1>
                   </div>
                   <div className="flex flex-col">
-                    <h1>teste1</h1>
-                    <h1>teste1</h1>
+                    <h1 className="text-gray-400">Contribuidores</h1>
+                    <h1 className="text-5xl">100k</h1>
                   </div>
                 </div>
               </div>
