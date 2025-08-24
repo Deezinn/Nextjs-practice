3f9cbfa56a908c328ea3749fa5a9fa907589dfe7
deezinn <developer.andrelz@gmail.com>
2025-08-24 18:20:47 -0300

    feat: new style to main page

--- easy-english/src/app/page.jsx
+++ easy-english/src/app/page.jsx
@@ -1,71 +1,75 @@
 import Image from "next/image";
 import personIcon from "@/assets/images/person_icon/person.jpeg";
 import { Icon } from "@iconify/react";
-
+import MicroCard from "@/components/microCard";
 
 export default function Home() {
   return (
-    <>
-      <div className="w-full h-auto p-2">
-        <div className="w-full h-auto grid grid-cols-[4fr_2fr]">
-          {/* Grade da esquerda */}
-          <div className="w-full h-auto bg-gray-200">
-            <h1>teste</h1>
-          </div>
-          {/* Grade da direita */}
-          <div className="w-full h-auto p-5 ">
-            <div className="w-full h-auto grid grid-rows-3 gap-5">
-              <div className="w-full h-auto flex flex-col text-white bg-gradient-to-r from-black/80 from-10% to-black to-90% p-5 gap-12 rounded-2xl">
-                {/* Parte de cima */}
-                <div className="w-full h-20 grid grid-cols-[1fr_2fr_1fr]">
-                  <div className="w-full h-auto flex justify-center items-center ">
-                    <div className="relative w-20 h-20">
-                      <Image
-                        src={personIcon}
-                        fill
-                        className=" object-cover rounded-full"
-                      />
-                    </div>
-                  </div>
-                  <div className="w-full h-auto flex flex-col">
-                    <h1 className="text-4xl">André Luiz</h1>
-                    <h1 className="text-md opacity-50">Developer</h1>
-                  </div>
-                  <div className="w-full h-auto flex justify-center mr-10">
-                    <Icon
-                      icon="svg-spinners:3-dots-move"
-                      width="60"
-                      height="60"
-                      className="text-white"
+    <div className="w-full h-auto p-2">
+      <div className="w-full h-auto grid grid-cols-[4fr_2fr]">
+        {/* Grade da esquerda */}
+        <div className="w-full h-auto bg-gray-200">
+          <h1>teste</h1>
+        </div>
+
+        {/* Grade da direita */}
+        <div className="w-full h-auto p-5">
+          <div className="w-full h-auto grid grid-rows-[0.5fr_1fr_0.5fr] gap-5">
+            {/* Primeiro card */}
+            <div className="w-full h-60 flex flex-col text-white bg-gradient-to-r from-black/80 from-10% to-black to-90% p-5 gap-8 rounded-2xl">
+              {/* Parte de cima */}
+              <div className="w-full h-20 grid grid-cols-[1fr_2fr_1fr]">
+                <div className="w-full h-auto flex justify-center items-center">
+                  <div className="relative w-20 h-20">
+                    <Image
+                      src={personIcon}
+                      alt="Foto de perfil"
+                      fill
+                      className="object-cover rounded-full"
                     />
                   </div>
                 </div>
-
-                <div className="w-full h-auto flex flex-row justify-evenly items-center relative ">
-                  <div className="flex flex-col">
-                    <h1 className="text-gray-400">Score</h1>
-                    <h1 className="text-5xl">9.7</h1>
-                  </div>
-                  <div className="flex flex-col">
-                    <h1 className="text-gray-400">Investimento</h1>
-                    <h1 className="text-5xl">10.5K</h1>
-                  </div>
-                  <div className="flex flex-col">
-                    <h1 className="text-gray-400">Contribuidores</h1>
-                    <h1 className="text-5xl">100k</h1>
-                  </div>
+                <div className="w-full h-auto flex flex-col">
+                  <h1 className="text-4xl">André Luiz</h1>
+                  <h1 className="text-md opacity-50">Developer</h1>
+                </div>
+                <div className="w-full h-auto flex justify-center mr-10">
+                  <Icon
+                    icon="svg-spinners:3-dots-move"
+                    width="60"
+                    height="60"
+                    className="text-white"
+                  />
                 </div>
               </div>
-              <div className="w-full h-auto bg-blue-500">
-                <h1>teste</h1>
-              </div>
-              <div className="w-full h-auto bg-blue-500">
-                <h1>teste</h1>
+
+              {/* Estatísticas */}
+              <div className="w-full h-auto flex flex-row justify-evenly items-center">
+                <div className="flex flex-col">
+                  <h1 className="text-gray-400">Score</h1>
+                  <h1 className="text-5xl">9.7</h1>
+                </div>
+                <div className="flex flex-col">
+                  <h1 className="text-gray-400">Investimento</h1>
+                  <h1 className="text-5xl">10.5K</h1>
+                </div>
+                <div className="flex flex-col">
+                  <h1 className="text-gray-400">Contribuidores</h1>
+                  <h1 className="text-5xl">100k</h1>
+                </div>
               </div>
             </div>
+
+            {/* Segundo card */}
+            <div>
+              <MicroCard />
+            </div>
+
+            {/* Terceiro card */}
+            <div className="w-full h-72 bg-blue-500 rounded-2xl"></div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
---
+++ easy-english/src/components/microCard.jsx
@@ -0,0 +1,120 @@
+import Image from "next/image";
+
+export default function MicroCard() {
+  return (
+    <div className="w-full h-auto rounded-2xl space-y-5">
+      {/* Tabs */}
+      <div className="flex flex-row justify-evenly items-center">
+        <div className="flex flex-col justify-between items-center">
+          <h1>Subscribed</h1>
+          <div className="w-1/2 h-1 bg-orange-300 rounded-2xl"></div>
+        </div>
+        <h1>Upcoming</h1>
+        <h1>Possed</h1>
+      </div>
+
+      {/* Item 1 */}
+      <div className="grid grid-cols-[2fr_1fr] w-full h-20 shadow-2xl rounded-2xl delay-150">
+        <div className="flex flex-row gap-5 justify-baseline items-center text-start p-5">
+          <div className="flex justify-center items-center bg-gray-200 w-10 h-10 rounded-xl">
+            <h1>A</h1>
+          </div>
+          <div>
+            <h1>Basic English</h1>
+            <h1 className="text-gray-500">Verb to be</h1>
+          </div>
+        </div>
+        <div className="flex justify-end items-center p-5">
+          {/* círculo girando */}
+          <div className="relative flex justify-center items-center w-10 h-10">
+            <div
+              className="absolute inset-0 rounded-full animate-spin"
+              style={{
+                background:
+                  "conic-gradient(black 0% 50%, transparent 50% 100%)",
+              }}
+            />
+            <div className="flex justify-center items-center w-8 h-8 bg-white rounded-full z-10">
+              <h1 className="text-gray-500 text-sm">50</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Item 2 */}
+      <div className="grid grid-cols-[2fr_1fr] w-full h-20 hover:shadow-2xl rounded-2xl">
+        <div className="flex flex-row gap-5 justify-baseline items-center text-start p-5">
+          <div className="flex justify-center items-center bg-gray-200 w-10 h-10 rounded-xl">
+            <h1>A</h1>
+          </div>
+          <div>
+            <h1>Node</h1>
+            <h1 className="text-gray-500">ExpressJs</h1>
+          </div>
+        </div>
+        <div className="flex justify-end items-center p-5">
+          <div
+            className="flex justify-center items-center w-10 h-10 rounded-full relative"
+            style={{
+              background: "conic-gradient(black 0% 70%, transparent 70% 100%)",
+            }}
+          >
+            <div className="flex justify-center items-center w-8 h-8 bg-white rounded-full z-10">
+              <h1 className="text-gray-500 text-sm">70</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Item 3 */}
+      <div className="grid grid-cols-[2fr_1fr] w-full h-20 hover:shadow-2xl rounded-2xl">
+        <div className="flex flex-row gap-5 justify-baseline items-center text-start p-5">
+          <div className="flex justify-center items-center bg-gray-200 w-10 h-10 rounded-xl">
+            <h1>A</h1>
+          </div>
+          <div>
+            <h1>Html, css and Javascript</h1>
+            <h1 className="text-gray-500">Class and Object</h1>
+          </div>
+        </div>
+        <div className="flex justify-end items-center p-5">
+          <div
+            className="flex justify-center items-center w-10 h-10 rounded-full relative"
+            style={{
+              background: "conic-gradient(black 0% 25%, transparent 25% 100%)",
+            }}
+          >
+            <div className="flex justify-center items-center w-8 h-8 bg-white rounded-full z-10">
+              <h1 className="text-gray-500 text-sm">25</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Item 4 */}
+      <div className="grid grid-cols-[2fr_1fr] w-full h-20 hover:shadow-2xl rounded-2xl">
+        <div className="flex flex-row gap-5 justify-baseline items-center text-start p-5">
+          <div className="flex justify-center items-center bg-gray-200 w-10 h-10 rounded-xl">
+            <h1>A</h1>
+          </div>
+          <div>
+            <h1>Database</h1>
+            <h1 className="text-gray-500">MySql</h1>
+          </div>
+        </div>
+        <div className="flex justify-end items-center p-5">
+          <div
+            className="flex justify-center items-center w-10 h-10 rounded-full relative"
+            style={{
+              background: "conic-gradient(black 0% 90%, transparent 90% 100%)",
+            }}
+          >
+            <div className="flex justify-center items-center w-8 h-8 bg-white rounded-full z-10">
+              <h1 className="text-gray-500 text-sm">90</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
